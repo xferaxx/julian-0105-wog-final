a0e8eb0bc07fc070429eae64df5643b0109417b1
xferaxx <juliankh18@hotmail.com>
2024-07-29 06:30:07 +0300

    FINAL Commit

--- run.cmd
+++ run.cmd
@@ -2,8 +2,9 @@
 echo Pulling Docker image from Docker Hub...
 docker pull xferax/flask-app:1.2
 
-echo Running Docker container...
-docker run -d -p 5000:5000 --name flask_app xferax/flask-app:1.2
+echo Running Docker containers...
+docker run -d -p 5000:5000 --name flask_app_5000 xferax/flask-app:1.2
+docker run -d -p 8777:5000 --name flask_app_8777 xferax/flask-app:1.2
 
-echo Docker container is up and running.
+echo Docker container is up and running on PORT 5000 AND 8777.
 pause
